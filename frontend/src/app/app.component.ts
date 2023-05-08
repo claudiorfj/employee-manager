05fadfdd7883625a72974c55e8a10d42f974f797
Cláudio Fernandes <93228350+claudiorfj@users.noreply.github.com>
2023-05-08 16:47:41 +0100

    create getEmployees

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IEmployee } from './interfaces/IEmployee';
+import { EmployeeService } from './services/employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employee-manager-app';
+export class AppComponent implements OnInit{
+
+  employees: IEmployee[] = []
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.getEmployees()
+  }
+
+  getEmployees(){
+    this.employeeService.getEmployees().subscribe(
+      (res: IEmployee[]) => {
+        this.employees = res
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+
 }
